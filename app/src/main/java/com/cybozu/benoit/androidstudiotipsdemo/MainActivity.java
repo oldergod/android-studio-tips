a96a3ee925939e1b1d1befd388d680ac42c04f0c
benoit <oldering@gmail.com>
2016-08-16 14:08:48 +0900

    final steps

--- app/src/main/java/com/cybozu/benoit/androidstudiotipsdemo/MainActivity.java
+++ app/src/main/java/com/cybozu/benoit/androidstudiotipsdemo/MainActivity.java
@@ -6,18 +6,15 @@ import android.util.Log;
 import android.widget.TextView;
 
 /**
- * TODO(benoit) prepare code for building demo
- * 1.- Flavor with 21 as minSdkTarget
+ * 1.- Update Gradle
+ * 2.- Fix gradle error with heap upgrade
+ * 3.- Flavor with 21 as minSdkTarget
  *      Project Structure -> App -> Flavor
- * 2.- Update Gradle
- * 3.- Fix gradle error with heap upgrade
  * 4.- Enable instant run
  * 5.- Change VersionCode in the gradle.properties
  *      to show we should understand how changes affect the build
  */
 public class MainActivity extends AppCompatActivity {
-
-    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +22,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         TextView textView = (TextView) findViewById(R.id.textView);
-        textView.setText("");
+        textView.setText("皆様、元気か！？なるほど。");
     }
 }
